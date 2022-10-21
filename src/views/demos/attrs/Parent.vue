d6b2f975a0c125b4e5b485ac5c8c291d5da9a522
朱阳星 <zhuyangxing@enmonster.com>
2022-10-21 10:37:38 +0800

    添加element-plus,添加attrs的例子

---
+++ src/views/demos/attrs/Parent.vue
@@ -0,0 +1,12 @@
+<template>
+  <child id="root" class="test" name="张三" @confirm="getData" />
+  <div>这里是父组件</div>
+</template>
+
+<script lang="ts" setup>
+import child from './child.vue'
+
+const getData = () => {
+  console.log('log')
+}
+</script>
---
+++ src/views/demos/attrs/child.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    这里是子组件
+    <span>hello：{{ props.name }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<script lang="ts" setup>
+import {useAttrs} from 'vue'
+const props = defineProps(['name'])
+
+const attrs = useAttrs()
+console.log('attrs', attrs)
+</script>
